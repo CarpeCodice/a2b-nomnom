5c4ad9df6b997c0d1de932a8f1e99d5f0b34f64a
CarpeCodice <116901222+CarpeCodice@users.noreply.github.com>
2024-02-22 17:50:23 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var countdownDate = new Date("Jan 1, 2025 00:00:00").getTime();
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countdownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 *60 *60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // console.log(distance)
+
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".countdown").innerHTML = "Count down expired"
+    }
+
+})
+
+// console.log(countdownDate);
